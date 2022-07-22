a2e172dde190d2bf22f3d640461c48217b2b3073
MarcioGomes78 <mjgomes1978@gmail.com>
2022-07-22 15:08:43 -0300

    Criando OrderStatus, Order, OrderItem e testando

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,10 +2,15 @@ package aplication;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
 
 public class Program {
 
@@ -16,9 +21,49 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Client cli = new Client("Marcio", "marcio@gmail.com", sdf.parse("13/12/1990"));
+		System.out.println("Enter client data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.next();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next());
 		
-		System.out.println(cli);
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println();
+		System.out.println("########## **Order** ##########");
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		Order order = new Order(new Date(), status, client);
+		
+		System.out.println(); //Uma linha em branco
+		System.out.println("########## **OrderItems** ##########");
+		System.out.print("Hoe many items to this order: ");
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			System.out.println("Enter # " + (i + 1) + " item data: ");
+			System.out.print("Product name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			double productPrice = sc.nextDouble();
+			System.out.print("Quantity: ");
+			int quantity = sc.nextInt();
+						
+			Product product = new Product(productName, productPrice);
+			
+			OrderItem it = new OrderItem(quantity, productPrice, product);
+			
+			order.addItem(it);
+		}
+		
+		System.out.println();
+		System.out.println("##########**Final Order**##########");
+		System.out.println(order);
+		
 		sc.close();
 		
 	}
